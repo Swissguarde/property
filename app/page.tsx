9b9113b718eba4bf35f1c4c3a4abd0918fe61aef
Kolade Ayomide <ayomikolade2003@gmail.com>
2024-10-01 19:25:00 +0100

    init project, added structure

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "./components/hero";
+import InfoBoxes from "./components/info-boxes";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
+  );
+}

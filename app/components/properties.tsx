b96c04958a82e57ccf84ae86c46f05d3feb69b09
Kolade Ayomide <ayomikolade2003@gmail.com>
2024-10-22 17:18:59 +0100

    added properties comp

---
+++ app/components/properties.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState, useEffect } from "react";
+import PropertyCard from "./property-card";
+import Spinner from "./spinner";
+import Pagination from "./pagination";
+
+const Properties = () => {
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties?page=${page}&pageSize=${pageSize}`,
+        );
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await res.json();
+        setProperties(data.properties);
+        setTotalItems(data.total);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProperties();
+  }, [page, pageSize]);
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <section className="px-4 py-6">
+      <div className="container-xl m-auto px-4 py-6 lg:container">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {properties.map((property: IProperty) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+        <Pagination
+          page={page}
+          pageSize={pageSize}
+          totalItems={totalItems}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </section>
+  );
+};
+export default Properties;
